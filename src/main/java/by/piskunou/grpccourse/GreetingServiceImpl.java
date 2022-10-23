9f10cf731ab3bc2e1bd34e2035932274425d252d
cichan <6684177pt@gmail.com>
2022-10-23 23:29:22 +0300

    add streams

--- src/main/java/by/piskunou/grpccourse/GreetingServiceImpl.java
+++ src/main/java/by/piskunou/grpccourse/GreetingServiceImpl.java
@@ -8,12 +8,19 @@ import io.grpc.stub.StreamObserver;
 public class GreetingServiceImpl extends GreetingServiceImplBase {
 	@Override
 	public void greeting(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
-		System.out.println(request);
-		
-		HelloResponse response = HelloResponse.newBuilder()
-											  .setGreeting("Hello from server, " + request.getName())
-											  .build();	
-		responseObserver.onNext(response);
+		System.out.println(request);		
+		for (int i = 0; i < 10_000; i++) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			HelloResponse response = HelloResponse.newBuilder()
+												  .setGreeting("Hello from server, " + request.getName())
+												  .build();	
+			responseObserver.onNext(response);
+		}	
 		responseObserver.onCompleted();
 	}
 }
