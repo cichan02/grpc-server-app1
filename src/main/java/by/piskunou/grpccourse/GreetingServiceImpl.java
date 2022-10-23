d813aee77718fc72e60696c91b919c9e3911f269
cichan <6684177pt@gmail.com>
2022-10-23 23:13:27 +0300

    small update

--- src/main/java/by/piskunou/grpccourse/GreetingServiceImpl.java
+++ src/main/java/by/piskunou/grpccourse/GreetingServiceImpl.java
@@ -1,20 +1,18 @@
 package by.piskunou.grpccourse;
 
 import by.piskunou.grpccourse.GreetingServiceGrpc.GreetingServiceImplBase;
+import by.piskunou.grpccourse.GreetingServiceOuterClass.HelloRequest;
+import by.piskunou.grpccourse.GreetingServiceOuterClass.HelloResponse;
 import io.grpc.stub.StreamObserver;
 
 public class GreetingServiceImpl extends GreetingServiceImplBase {
 	@Override
-	public void greeting(GreetingServiceOuterClass.HelloRequest request,
-						 StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
+	public void greeting(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
 		System.out.println(request);
 		
-		GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass
-															.HelloResponse
-															.newBuilder()
-															.setGreeting("Hello from server, " + request.getName())
-															.build();
-		
+		HelloResponse response = HelloResponse.newBuilder()
+											  .setGreeting("Hello from server, " + request.getName())
+											  .build();	
 		responseObserver.onNext(response);
 		responseObserver.onCompleted();
 	}
